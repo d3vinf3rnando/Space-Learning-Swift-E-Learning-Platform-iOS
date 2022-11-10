c8c00b873a17404f96c802878682167925bf8ca5
Devin Fernando <103265400+d3vinf3rnando@users.noreply.github.com>
2022-11-10 11:54:51 +0530

    Initial Commit

---
+++ Space_Learning/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Space_Learning
+//
+//  Created by Devin Fernando on 2022-11-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Space_Learning/Space_LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  Space_LearningApp.swift
+//  Space_Learning
+//
+//  Created by Devin Fernando on 2022-11-10.
+//
+
+import SwiftUI
+
+@main
+struct Space_LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
